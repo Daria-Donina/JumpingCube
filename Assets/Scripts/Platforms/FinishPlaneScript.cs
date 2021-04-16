53e50c95b45aec541e848e80e36dbb5b1fefdfd8
Matvei-Fadeev <matwei.fadeew@gmail.com>
2021-04-16 19:38:11 +0300

    Add levels

--- Assets/Scripts/Platforms/FinishPlaneScript.cs
+++ Assets/Scripts/Platforms/FinishPlaneScript.cs
@@ -5,5 +5,5 @@ public class FinishPlaneScript : MonoBehaviour
     [SerializeField]
     private string nextSceneName;
 
-	void OnCollisionEnter(Collision collision) => Loader.LoadScene(nextSceneName);
+    void OnTriggerEnter(Collider other) => Loader.LoadScene(nextSceneName);
 }
