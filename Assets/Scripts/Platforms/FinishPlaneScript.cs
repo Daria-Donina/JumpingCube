4a8b0aca6bc97be01916cedbbcb35ef836914fe6
Matvei-Fadeev <matwei.fadeew@gmail.com>
2021-04-19 13:52:26 +0300

    Add the delay before loading the next scene

--- Assets/Scripts/Platforms/FinishPlaneScript.cs
+++ Assets/Scripts/Platforms/FinishPlaneScript.cs
@@ -1,9 +1,20 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
-public class FinishPlaneScript : MonoBehaviour
-{
+public class FinishPlaneScript : MonoBehaviour {
     [SerializeField]
-    private string nextSceneName;
+    private string nextSceneName = "DefaultScene";
 
-    void OnTriggerEnter(Collider other) => Loader.LoadScene(nextSceneName);
+    [SerializeField]
+    private float timeBeforeLoading = 0.2f;
+
+    void OnTriggerEnter(Collider other) {
+        StartCoroutine(DelayedLoading());
+    }
+
+    IEnumerator DelayedLoading() {
+        yield return new WaitForSeconds(timeBeforeLoading);
+        Loader.LoadScene(nextSceneName);
+    }
 }
+
