b966ca956888482a2e9dacaca1cbed53550e4422
Matvei-Fadeev <matwei.fadeew@gmail.com>
2021-04-21 19:33:39 +0300

    Add snowfall effect
    On the snowfall maps can to use the snowfall effect

---
+++ Assets/Snowfall.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snowfall : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _player;
+
+    [SerializeField]
+    private float _height;
+
+    private void FixedUpdate() {
+        Vector3 newPosition = _player.transform.position;
+        newPosition.y = _height;
+        transform.position = newPosition;
+    }
+}   
